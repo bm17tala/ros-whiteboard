09c2be74d381ceda5aaba48d173d5195d4dabe57
bm17tala <bm17tala@siena.edu>
2024-12-04 11:43:07 -0500

    alter_speeds

--- scripts/paint.py
+++ scripts/paint.py
@@ -207,8 +207,8 @@ class PaintGUI:
 
                 angle_rad = math.atan2(dy, dx)
 
-                move_cmd.linear.x = 0.25 * math.cos(angle_rad)  # Forward motion along the angle
-                move_cmd.linear.y = 0.1 * math.sin(angle_rad)  # Forward motion along the angle
+                move_cmd.linear.x = 0.1 * math.cos(angle_rad)  # Forward motion along the angle
+                move_cmd.linear.y = 0.5 * math.sin(angle_rad)  # Forward motion along the angle
                 move_cmd.angular.z = 0
 
                 print("x: ", move_cmd.linear.x, " y: ", move_cmd.linear.y, " dx: ", dx, " dy: ", dy, " angle_rad: ", angle_rad, " distance: ", distance) 
