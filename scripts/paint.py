0daefb78fd5ba76402ffcef5567acd064e519b71
bm17tala <bm17tala@siena.edu>
2024-12-04 12:21:39 -0500

    adj_distance

--- scripts/paint.py
+++ scripts/paint.py
@@ -215,7 +215,7 @@ class PaintGUI:
 
                 main.pub.publish(move_cmd)
 
-                rospy.sleep(distance / 62)
+                rospy.sleep(distance / 70)
 
                 # move_cmd.linear.x = 0
                 # move_cmd.linear.y = 0
